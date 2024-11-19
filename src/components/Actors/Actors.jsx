8f71ef529197d165be8e7258b63fb7331de220cb
hassaanfaridi <fireside.faridi@gmail.com>
2024-11-19 18:10:25 +0500

    Initial commit

---
+++ src/components/Actors/Actors.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Actors = () => {
+  return (
+    <h1>Actors</h1>
+  )
+}
+
+export default Actors
---
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MovieInformation = () => {
+  return (
+    <h1>Movie Information</h1>
+  );
+}
+
+export default MovieInformation
